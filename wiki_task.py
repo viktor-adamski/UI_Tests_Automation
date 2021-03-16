467373822dc2d41b944b670cf140c0a95932085a
Viktor Adamski <Vito@MacBook-Air-Viktor.local>
2021-03-16 14:46:35 +0100

    Test Case Add

--- wiki_task.py
+++ wiki_task.py
@@ -2,54 +2,57 @@ import time
 from selenium import webdriver
 from random import shuffle
 
-# listOfLinks - there I'm collecting links, which the program clicked
-# title - there I'm saving title of visited page, that I can to define, that the program is appeared on necessary page
-# noneType - is used for detecting that the links is clickable
 
-listOfLinks, title, noneType = [], '', type(None)
+class Wiki:
+    wiki_main_url = 'https://en.wikipedia.org/wiki/Main_Page'
+    listOfLinks = []
+    title = ''
+    noneType = type(None)
 
-# Activating WebDriver
+    def __init__(self, path_to_drive, search_page):
+        self.search_page = search_page
+        self.driver = webdriver.Chrome(executable_path=path_to_drive)
+        self.driver.maximize_window()
+        self.driver.get(self.wiki_main_url)
 
-driver = webdriver.Chrome(executable_path='C:/Users/kremen/OneDrive/Рабочий стол/chromedriver')
-driver.maximize_window()
-driver.get('https://en.wikipedia.org/wiki/Main_Page')
+    def get_elements(self):
+        if not self.title:
+            elements = self.driver.find_elements_by_css_selector("div.mp-bordered+div a")
+            shuffle(elements)
+            return elements
+        else:
+            return self.driver.find_elements_by_css_selector("div.mw-body-content p a")
 
-# loop for finding necessary the page, which will working while not finding necessary the page
-
-while title != 'Philosophy':
-    # if title = '', that means that the program is on home page of wiki and we need to find all link of articles
-    # in another case the program will take all links from article
-
-    if not title:
-        elements = driver.find_elements_by_css_selector("div.mp-bordered+div a")
-
-        # using 'shuffle' to get random article
-
-        shuffle(elements)
-    else:
-        elements = driver.find_elements_by_css_selector("div.mw-body-content p a")
-
-    # loop where the program is checking that links is normal links without links on files, images, is clickable and etc
-    # and if link is as we need, then program clicks on it and save this link to variable ''listOfLinks''
-    # and save the title of article to variable 'title'
-    # in another case, the program skip this link
-
-    for i in range(0, len(elements)):
-        if type(elements[i].get_attribute("href")) is noneType or elements[i].get_attribute("href").find('#') > -1 \
-                or elements[i].get_attribute("href").find('/wiki/') == -1 \
-                or listOfLinks.count(elements[i].get_attribute("href").lower()) > 0 \
+    def get_data(self, elements, i):
+        if type(elements[i].get_attribute("href")) is self.noneType or elements[i] \
+                .get_attribute("href").find('#') > -1 or elements[i].get_attribute("href").find('/wiki/') == -1 \
+                or self.listOfLinks.count(elements[i].get_attribute("href").lower()) > 0 \
                 or elements[i].get_attribute("href").find('https://en.wikipedia.org/') == -1 \
                 or elements[i].get_attribute("href").find('disambiguation') > -1 \
                 or elements[i].get_attribute("href").find(':', 7) > -1 or not elements[i].is_displayed():
-            continue
+            return 1
         else:
-            listOfLinks.append(elements[i].get_attribute("href").lower())
+            self.listOfLinks.append(elements[i].get_attribute("href").lower())
             elements[i].click()
-            title = driver.find_elements_by_css_selector("h1.firstHeading")[0].get_attribute('innerHTML')
+            self.title = self.driver.find_elements_by_css_selector("h1.firstHeading")[0] \
+                .get_attribute('innerHTML')
             time.sleep(1)
-            break
+            return 0
 
-# printing into output amount of visiting pages and close browser
+    def find_page(self):
+        while self.title != self.search_page:
+            elements = self.get_elements()
 
-print(len(listOfLinks))
-driver.quit()
+            for i in range(0, len(elements)):
+                if not self.get_data(elements, i):
+                    break
+
+        self.printing_result()
+
+    def printing_result(self):
+        print(len(self.listOfLinks))
+        self.driver.quit()
+
+
+wiki = Wiki('/Users/Vito/Desktop/Selenium/Wiki_task/chromedriver', 'Philosophy')
+wiki.find_page()
